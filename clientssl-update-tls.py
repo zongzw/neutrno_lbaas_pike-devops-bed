4901eb000f817dd37c13203c92a749e76a461cf3
Andrew Zong <a.zong@f5.com>
2020-11-26 03:20:06 -0800

    Add  full  tests  for  clientssl profiles

--- clientssl-update-tls.py
+++ clientssl-update-tls.py
@@ -1,12 +1,15 @@
 import requests
 import json
 import sys
+import base64
 
 os_host = "10.145.73.123"
 os_project_id = "e04af77e23be443989be14e22240ea75"
 os_username = "admin"
 os_password = "f543e5c237e54891"
 listener_id = "56e0b93c-0485-4695-8614-d643bc800238"
+bigip_credential = "admin:admin"
+bigip_host = "10.145.68.11"
 
 # the least container count for later tests
 least_container_num = 3
@@ -97,7 +100,7 @@ def get_secret_containers():
     except Exception as e:
         raise e
 
-def update_listener_with_tls(listener_id, name, default_tls_container_id, sni_container_refs):
+def update_listener_with_tls(listener_id, default_tls_container_id, sni_container_refs, name=""):
     tls_update_url = "http://%s:9696/v2.0/lbaas/listeners/%s" % (os_host, listener_id)
 
     payload = {
@@ -111,9 +114,10 @@ def update_listener_with_tls(listener_id, name, default_tls_container_id, sni_co
         'Content-Type': 'application/json',
         'X-Auth-Token': authed_token
     }
+    payload_data = json.dumps(payload)
 
     try:
-        response = requests.request("PUT", tls_update_url, headers=headers, data = payload)
+        response = requests.request("PUT", tls_update_url, headers=headers, data = payload_data)
         if int(response.status_code / 200) == 1:
             print(json.dumps(json.loads(response.text.encode('utf8')), indent=2))
             return json.loads(response.text.encode('utf8'))
@@ -123,15 +127,69 @@ def update_listener_with_tls(listener_id, name, default_tls_container_id, sni_co
     except Exception as e:
         raise e
 
-def get_bigip_profiles():
-    pass
+def get_bigip_profiles(listener_id):
+    authorization = "%s" % base64.b64encode(bigip_credential)
+
+    profiles_url = "https://%s/mgmt/tm/ltm/virtual/~Project_%s~Project_%s/profiles" % (bigip_host, os_project_id, listener_id)
+
+    payload = {}
+    headers = {
+        'content-type': 'application/json',
+        'Authorization': 'Basic %s' % authorization
+    }
+
+    try:
+        response = requests.request("GET", profiles_url, verify=False, headers=headers, data = payload)
+        
+        if int(response.status_code / 200) == 1:
+            print(json.dumps(json.loads(response.text.encode('utf8')), indent=2))
+            return json.loads(response.text.encode('utf8'))['items']
+        else:
+            print("failed to update listener: %d, %s" % (response.status_code, response.text.encode('utf-8')))
+            sys.exit(1)
+    except Exception  as e:
+        raise e
+
+def check_listener_with_tls(listener_id, def_tls, sni_tls):
+    
+    listener = get_listener(listener_id)
+    if listener['listener']['default_tls_container_ref'] != def_tls:
+        raise Exception("listener default_tls_container_ref doesn't match!")
+    
+    if sorted(listener['listener']['sni_container_refs']) !=  sorted(sni_tls):
+        raise  Exception("listener sni_container_refs doesn't match!")
+
+    profiles = get_bigip_profiles(listener_id)
+
+    def is_ssl_profile(n):
+        return n['context'] == 'clientside'
+    ssl_profiles = filter(is_ssl_profile, profiles)
+    
+    if len([def_tls] +sni_tls) != len(ssl_profiles):
+        raise Exception("The length of ssl profiles doesn't match.")
+
+    def id_found(id):
+        for n in profiles:
+            if n['fullPath'].find(id) != -1:
+                return True
+        return False
+
+    def_tls_id = def_tls.split('/')[-1]
+    if not id_found(def_tls_id):
+        raise  Exception("default tls id  %s cannot be found in profiles: %s"  % (def_tls_id,  profiles))
+    
+    for n in sni_tls:
+        sni_tls_id  =n.split('/')[-1]
+        if not id_found(sni_tls_id):
+            raise Exception("sni  tls  id  %s cannot  be  found  in profiles: %s"%(sni_tls_id, profiles))
+    
+
 
 
 # ============================= main logic =============================
 
 auth_token()
 
-listener = get_listener(listener_id)
 containers = get_secret_containers()
 
 if containers['total'] < least_container_num:
@@ -139,33 +197,31 @@ if containers['total'] < least_container_num:
     sys.exit(1)
 
 containers = {
-    'A' = containers['containers'][0]['container_ref'],
-    'B' = containers['containers'][1]['container_ref'],
-    'C' = containers['containers'][2]['container_ref']
+    'A': containers['containers'][0]['container_ref'],
+    'B': containers['containers'][1]['container_ref'],
+    'C': containers['containers'][2]['container_ref']
 }
-# tests: The pairs (keys and values) are composed with 'A' 'B' 'C', mean container ids as shown above
+# tests: 'A' 'B' 'C', mean container ids as shown above
 # "A B" The first letter means default_tls_container_id, the later letter(s) means sni_container_refs
-# The keys are original state, the values are changed-to state.
+# In tests array, listeners' tls setting transitions from tests[0] to tests[1],  to ... tests[n]
 # i.e. 
-#   "A B": "C B" means: change default_tls_container_id from A to C, and keep sni_container_refs unchanged.
+#   ..., "A B", "C B", ... means: change default_tls_container_id from A to C, and keep sni_container_refs unchanged.
 
 tests = [
-    ("A", "B"),
-    ("A B", "C B"),
-    ("A B", "A C"),
-    ("A B", "A"),
-    ("A B", "A B C"),
-    ("A B C", "A")
+    "A", "B", "A", "A B", "C B", "A B", "A C", "A B", "A B C", "A B", "C"
 ]
 
-for (k, v) in tests:
-    print(k, v)
-    origins = k.split(' ')
-    targets = v.split(' ')
-    print(origins, targets)
+tls = tests[0].split(' ')
+def_tls = containers[tls[0]]
+sni_tls = [containers[n] for n in tls[1:]]
+ls = update_listener_with_tls(listener_id, def_tls, sni_tls, name=tests[0])
+check_listener_with_tls(listener_id, def_tls, sni_tls)
 
-    def_tls = containers[origins[0]]
-    sni_tls = containers[origins[1:]]
-    ls = update_listener_with_tls(listener_id, k, def_tls, sni_tls)
-    get_bigip_profiles()
+for t in tests[1:]:
+    print(t)
+    tls = t.split(' ')
+    def_tls = containers[tls[0]]
+    sni_tls = [containers[n] for n in tls[1:]]
+    ls = update_listener_with_tls(listener_id, def_tls, sni_tls, name=t)
+    check_listener_with_tls(listener_id, def_tls, sni_tls)
 
